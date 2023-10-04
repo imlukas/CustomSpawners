9bef44a42aac1e6beee6dd026a281218d140cfe4
Lukas Pinheiro <lppinheiro1234@gmail.com>
2023-10-04 14:36:37 +0100

    Fixed spawners working while player is offline

--- src/main/java/dev/imlukas/ultraspawners/listener/ConnectionListener.java
+++ src/main/java/dev/imlukas/ultraspawners/listener/ConnectionListener.java
@@ -1,6 +1,9 @@
 package dev.imlukas.ultraspawners.listener;
 
 import dev.imlukas.ultraspawners.UltraSpawnersPlugin;
+import dev.imlukas.ultraspawners.data.PlayerData;
+import dev.imlukas.ultraspawners.impl.InstancedSpawner;
+import dev.imlukas.ultraspawners.registry.GeneralSpawnerRegistry;
 import dev.imlukas.ultraspawners.registry.PlayerDataRegistry;
 import dev.imlukas.ultraspawners.storage.FileDatabase;
 import org.bukkit.event.EventHandler;
@@ -10,12 +13,15 @@ import org.bukkit.event.player.PlayerQuitEvent;
 
 public class ConnectionListener implements Listener {
 
+
     private final FileDatabase fileDatabase;
     private final PlayerDataRegistry playerDataRegistry;
+    private final GeneralSpawnerRegistry spawnerRegistry;
 
     public ConnectionListener(UltraSpawnersPlugin plugin) {
         this.fileDatabase = plugin.getFileDatabase();
         this.playerDataRegistry = plugin.getPlayerDataRegistry();
+        this.spawnerRegistry = plugin.getSpawnerRegistry();
     }
 
     @EventHandler
@@ -26,7 +32,17 @@ public class ConnectionListener implements Listener {
 
     @EventHandler
     public void onLeave(PlayerQuitEvent event) {
+        for (InstancedSpawner spawner : spawnerRegistry.getAllSpawners()) {
+            for (PlayerData playerData : spawner.getPlayersInRange()) {
+                if (playerData.getPlayerId().equals(event.getPlayer().getUniqueId())) {
+                    spawner.removePlayerInRange(playerData);
+                }
+            }
+        }
+
+
         playerDataRegistry.unregister(event.getPlayer().getUniqueId());
         fileDatabase.getFileManager().remove(event.getPlayer().getUniqueId());
+
     }
 }
